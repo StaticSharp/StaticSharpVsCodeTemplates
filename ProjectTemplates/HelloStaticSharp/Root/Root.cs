491d1e3624840c8e9b364f20ec94c657a5e21ed1
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-07-03 13:42:05 +0300

    1st project template

---
+++ ProjectTemplates/HelloStaticSharp/Root/Root.cs
@@ -0,0 +1,19 @@
+﻿using StaticSharp;
+
+namespace HelloStaticSharp.Root
+{
+    [Representative]
+    public partial class Root : PageBase
+    {
+        public override string Title => "HelloStaticSharp - Home page";
+
+        public override Inlines? Description => "Home page of StaticSharp sample project";
+
+        public override Blocks Content => new() {
+            """
+            Text text text text text text text text text text text text text
+            text text text text text text text text text text text text text.
+            """
+        };
+    }
+}
